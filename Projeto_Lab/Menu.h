85506b612092c482b1e803d3bf928f0acbf8b878
Camila Figueiredo <68570532+CamilaFSilva10@users.noreply.github.com>
2021-12-03 20:25:14 -0300

    Delete Projeto_Lab directory

--- Projeto_Lab/Menu.h
+++
@@ -1,7 +0,0 @@
-#ifndef MENU_H_INCLUDED
-#define MENU_H_INCLUDED
-//definições de protótipos das funções e variaveis globais
-int MenuInicial();
-char restaurante[20], hotel[20], estacionamento[20];
-
-#endif // MENU_H_INCLUDED
